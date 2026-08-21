22d8cb3b79c3ff53f59c8a741b99c10152736551
luizjrlopes <luizjunior.lopes@gmail.com>
2026-08-20 22:45:06 -0300

    init

---
+++ convite-data.js
@@ -0,0 +1,10 @@
+// EDITE SOMENTE ESTE ARQUIVO PARA PERSONALIZAR O CONVITE.
+window.CONVITE = {
+  menina: 'Maria Luiza',
+  menino: 'Matteo',
+  data: '07 JULHO',
+  horario: 'ÀS 16:00 HORAS',
+  confirmarPresenca: '', // Ex.: 'https://wa.me/5511999999999'
+  localizacaoFesta: '',  // Ex.: 'https://maps.google.com/?q=...'
+  sugestoesPresentes: '' // Ex.: 'https://lista-de-presentes.com/...'
+};
